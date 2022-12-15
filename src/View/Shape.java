fd94d2d1896b65828ca675b6ea18a169e4545e4b
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-12-15 13:42:11 +0100

    byte till den nyare har nu en old src

--- src/View/Shape.java
+++ src/View/Shape.java
@@ -7,5 +7,22 @@ package View;
  * @author Magnus Silverdal
  */
 public abstract class Shape {
+    private int width = 500;
+    private int height = 500;
 
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
