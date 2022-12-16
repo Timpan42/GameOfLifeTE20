eac07de44093a040b8c84b9d198d4ae156facf4f
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-12-16 13:38:10 +0100

    försöker börja med hur point kan kålla runt sig med reglerna som finns, den funkar inte!

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,16 +1,43 @@
 package Model;
 import View.*;
 
+import java.util.ArrayList;
+
 public class Model {
+    int x;
+    int y;
+    ArrayList<Integer> Points = new ArrayList<>();
     public Model(int width, int height) {
 
     }
 
     public void update() {
+        getRules();
     }
 
     public Shape[] getShapes() {
-        Point[] points = {new Point(5,5)};
-        return (Shape[])points;
+        Points = {new Point(10,10), new Point(11,10), new Point(11,11)};
+        return (Shape[])Points;
+    }
+
+    public void getRules(){
+        for (int i = 0; i < Points.size(); i++) {
+            for (int j = 0; j < Points.size(); j++) {
+                if (Point[i] == Point[j]){
+                    //don't move
+                }
+                if(){
+                    // if alone dies
+                }
+                if(){
+                    //if four cells dies
+                }
+                if(){
+                    //if have one or two neighbor liv
+                }
+            }
+
+
+        }
     }
 }
