de4d9edd3e19b016429bb8627c85ce2c67cf0d01
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2023-01-19 19:23:09 +0100

    Kan ändra på cellerna från en funktion som uppdateras i update

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,25 +1,51 @@
 package Model;
 import View.*;
-
 import java.util.ArrayList;
 
 public class Model {
-    int x;
-    int y;
-    public Cell cell;
+
+    int width, height, alone, live, abundent;
+    private int x;
+    private int y;
+    ArrayList<Point> points = new ArrayList<>();
+    ArrayList<Point> cells = new ArrayList<>();
+
+
     public Model(int width, int height) {
+        this.width = width;
+        this.height = height;
+        alone = 1;
+        live = 3;
+        abundent = 4;
+        x++;
+        y += 1;
+        cells = points;
 
-
-        cell = new Cell(20,20,0,true);
+        points.add(new Point(5,5));
+        points.add(new Point(6,5));
+        points.add(new Point(7,5));
     }
 
     public void update() {
-
+        rules();
+        updateCells();
     }
 
     public Shape[] getShapes() {
-        Point[] points = {cell};
-        return (Shape[])points;
+        Point[] pixelsToDisplay = new Point[points.size()];
+        points.toArray(pixelsToDisplay);
+        return (Shape[])pixelsToDisplay;
     }
 
-        }
+    public void updateCells(){
+        Point[] cells = (Point[]) getShapes();
+        cells[0].setX(x);
+        cells[1].setY(y);
+        cells[2].setX(15);
+
+    }
+
+    public void rules(){
+
+    }
+}
